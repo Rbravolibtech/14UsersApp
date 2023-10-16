30562013c0100fcdb6002bfcc06e1a4e68c8e8e0
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-16 12:12:40 -0600

    aded styling and added code to userfinder file

---
+++ src/components/UserFinder.js
@@ -0,0 +1,66 @@
+import { Fragment, Component } from "react";
+
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+
+const DUMMY_USERS = [
+	{ id: "u1", name: "Max" },
+	{ id: "u2", name: "Manuel" },
+	{ id: "u3", name: "Julie" },
+	{ id: "u3", name: "Julie" },
+	{ id: "u3", name: "robert" },
+	{ id: "u3", name: "Jax" },
+	{ id: "u3", name: "Jude" },
+];
+
+class UserFinder extends Component {
+	constructor() {
+		super();
+		this.state = {
+			filteredUsers: [],
+			searchTerm: "",
+		};
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.searchTerm !== this.state.searchTerm) {
+			this.setState({
+				filteredUsers: DUMMY_USERS.filter((user) =>
+					user.name.includes(this.state.searchTerm),
+				),
+			});
+		}
+	}
+
+	searchChangeHandler(event) {
+		this.setState({ searchTerm: event.target.value });
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<div className={classes.finder}>
+					<input type="search" onChange={this.searchChangeHandler.bind(this)} />
+				</div>
+				<Users users={this.state.filteredUsers} />
+			</Fragment>
+		);
+	}
+}
+
+// // const UserFinder = () => {
+// // 	const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+// // 	const [searchTerm, setSearchTerm] = useState("");
+
+// // 	useEffect(() => {
+// // 		setFilteredUsers(
+// // 			DUMMY_USERS.filter((user) => user.name.includes(searchTerm)),
+// // 		);
+// // 	}, [searchTerm]);
+
+// // 	const searchChangeHandler = (event) => {
+// // 		setSearchTerm(event.target.value);
+// // 	};
+// };
+
+export default UserFinder;
